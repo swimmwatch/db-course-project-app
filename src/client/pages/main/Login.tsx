076a6499b6d1f3cd507fa2b92776244a11bb61fd
Dmitry Vasiliev <vasiliev.dim2014@gmail.com>
2020-08-30 20:53:34 +0300

    Change structure of login form

--- src/client/pages/main/Login.tsx
+++ src/client/pages/main/Login.tsx
@@ -10,16 +10,13 @@ export default class Login extends React.Component {
         return (
             <Container className="p-3">
                 <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Group controlId="formBasicLogin">
+                        <Form.Label>Login</Form.Label>
+                        <Form.Control type="text" placeholder="Enter login" />
                     </Form.Group>
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
                     </Form.Group>
                     <Button variant="primary" type="submit">Submit</Button>
                 </Form>
