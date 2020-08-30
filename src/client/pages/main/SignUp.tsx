d1bb83035a45d8567e1e85d41555fc4de3265f83
Dmitry Vasiliev <vasiliev.dim2014@gmail.com>
2020-08-30 20:50:52 +0300

    Change structure of sign up form

--- src/client/pages/main/SignUp.tsx
+++ src/client/pages/main/SignUp.tsx
@@ -4,7 +4,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 
-export default class Login extends React.Component {
+export default class SignUp extends React.Component {
     // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
     render() {
         return (
@@ -21,9 +21,6 @@ export default class Login extends React.Component {
                     <Form.Group controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
                     </Form.Group>
                     <Button variant="primary" type="submit">Submit</Button>
                 </Form>
