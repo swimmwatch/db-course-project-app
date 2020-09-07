852a80ab529e6869a9d1a2fae835e96d576a11f5
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-07 09:26:55 +0300

    Create auth controller

---
+++ src/controllers/auth.ts
@@ -0,0 +1,63 @@
+import {
+    Request,
+    Response,
+    NextFunction
+} from "express";
+import * as bcrypt from "bcrypt";
+import {BAD_REQUEST, FORBIDDEN} from "http-status-codes";
+
+import User from "../models/User";
+
+export const signup = async (req: Request, res: Response, next: NextFunction) => {
+    const { login, ...credentials } = req.body;
+
+    let user;
+    try {
+        user = await User.findOne({ where: { login } });
+    } catch (error) {
+        console.error(error);
+
+        res.status(FORBIDDEN);
+    }
+
+    if (user !== null) {
+        next({
+            status: BAD_REQUEST,
+            message: "User with such name already exists"
+        });
+    } else {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const password = await User.hashPassword(credentials.password);
+
+        await User.create({ ...credentials, password, login });
+
+        res.json("success");
+    }
+};
+
+export const signin = async (req: Request, res: Response, next: NextFunction) => {
+    const { login, ...credentials } = req.body;
+
+    let user;
+    try {
+        user = await User.findOne({ where: { login } });
+    } catch (error) {
+        console.error(error);
+
+        res.status(FORBIDDEN);
+    }
+
+    if (!user) {
+        next({
+            status: BAD_REQUEST,
+            message: "User with such name not found"
+        });
+    } else {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const isRight = await bcrypt.compare(credentials.password, user.password);
+
+        res.send(isRight);
+    }
+};
