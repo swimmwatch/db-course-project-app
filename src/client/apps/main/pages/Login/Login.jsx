a6e1ddd32cd42317e8fda1f9834b6cb31bfe8b84
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-15 22:12:37 +0300

    Done design of login page

--- src/client/apps/main/pages/Login/Login.jsx
+++ src/client/apps/main/pages/Login/Login.jsx
@@ -3,13 +3,16 @@ import * as React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import LinkContainer from "react-router-bootstrap/lib/LinkContainer";
+
+import "./style.scss";
 
 const Login = () => {
     return (
         <Container className="p-3 col-lg-4 offset-lg-4">
-            <h2>Login form</h2>
+            <h2 className="main-login-form__title">Login form</h2>
             <Form>
-                <Form.Group controlId="main-form-login">
+                <Form.Group controlId="main-login-form">
                     <Form.Label>Login:</Form.Label>
                     <Form.Control type="text"
                                   placeholder="Enter login"
@@ -21,10 +24,15 @@ const Login = () => {
                                   placeholder="Enter password"
                                   name="password"/>
                 </Form.Group>
+                <Form.Group controlId="main-form-checkbox">
+                    <Form.Check type="checkbox" label="Remember me"/>
+                </Form.Group>
                 <Button variant="primary"
                         type="submit"
                         block>Submit</Button>
-                <p className="form_forgot-password">Forgot password?</p>
+                <LinkContainer to="#">
+                    <a className="main-login-form__forgot-password">Forgot password?</a>
+                </LinkContainer>
             </Form>
         </Container>
     );
