96dada8dcfe61c903ef37d7cfe46ae51df9f5b7a
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-16 08:47:41 +0300

    Create basic design for main sign up form

--- src/client/apps/main/pages/SignUp/SignUp.jsx
+++ src/client/apps/main/pages/SignUp/SignUp.jsx
@@ -4,24 +4,35 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 
+import "./style.scss";
+
 export default class SignUp extends React.Component {
     render() {
         return (
-            <Container className="p-3">
+            <Container className="p-3 col-lg-4 offset-lg-4">
+                <h2 className="main-signup-form__title">Sign Up form</h2>
                 <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" name="email" />
+                    <Form.Group controlId="main-signup-form__email">
+                        <Form.Label>Email address:</Form.Label>
+                        <Form.Control type="email"
+                                      placeholder="Enter email"
+                                      name="email" />
                     </Form.Group>
-                    <Form.Group controlId="formBasicLogin">
-                        <Form.Label>Login</Form.Label>
-                        <Form.Control type="text" placeholder="Enter login" name="login" />
+                    <Form.Group controlId="main-signup-form__login">
+                        <Form.Label>Login:</Form.Label>
+                        <Form.Control type="text"
+                                      placeholder="Enter login"
+                                      name="login" />
                     </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" name="password" />
+                    <Form.Group controlId="main-signup-form__password">
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control type="password"
+                                      placeholder="Enter password"
+                                      name="password" />
                     </Form.Group>
-                    <Button variant="primary" type="submit">Submit</Button>
+                    <Button variant="primary"
+                            type="submit"
+                            block>Submit</Button>
                 </Form>
             </Container>
         );
