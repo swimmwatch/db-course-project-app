857963e83d4e160ef9ed75f7d15caa67b7e6f242
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-16 08:36:52 +0300

    Change id labels from main login form

--- src/client/apps/main/pages/Login/Login.jsx
+++ src/client/apps/main/pages/Login/Login.jsx
@@ -12,19 +12,19 @@ const Login = () => {
         <Container className="p-3 col-lg-4 offset-lg-4">
             <h2 className="main-login-form__title">Login form</h2>
             <Form>
-                <Form.Group controlId="main-login-form">
+                <Form.Group controlId="main-login-form__login">
                     <Form.Label>Login:</Form.Label>
                     <Form.Control type="text"
                                   placeholder="Enter login"
                                   name="login"/>
                 </Form.Group>
-                <Form.Group controlId="main-form-password">
+                <Form.Group controlId="main-login-form__password">
                     <Form.Label>Password:</Form.Label>
                     <Form.Control type="password"
                                   placeholder="Enter password"
                                   name="password"/>
                 </Form.Group>
-                <Form.Group controlId="main-form-checkbox">
+                <Form.Group controlId="main-login-form__checkbox">
                     <Form.Check type="checkbox" label="Remember me"/>
                 </Form.Group>
                 <Button variant="primary"
