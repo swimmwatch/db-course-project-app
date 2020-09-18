959b0c17122645281ff43905a351ef015543fa5a
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-18 19:12:18 +0300

    Update labels style in sign up form

--- src/client/apps/main/pages/SignUp/SignUp.jsx
+++ src/client/apps/main/pages/SignUp/SignUp.jsx
@@ -13,19 +13,19 @@ export default class SignUp extends React.Component {
                 <h2 className="main-signup-form__title">Sign Up form</h2>
                 <Form>
                     <Form.Group controlId="main-signup-form__email">
-                        <Form.Label>Email address:</Form.Label>
+                        <Form.Label className="main-signup-form__label">Email address:</Form.Label>
                         <Form.Control type="email"
                                       placeholder="Enter email"
                                       name="email" />
                     </Form.Group>
                     <Form.Group controlId="main-signup-form__login">
-                        <Form.Label>Login:</Form.Label>
+                        <Form.Label className="main-signup-form__label">Login:</Form.Label>
                         <Form.Control type="text"
                                       placeholder="Enter login"
                                       name="login" />
                     </Form.Group>
                     <Form.Group controlId="main-signup-form__password">
-                        <Form.Label>Password:</Form.Label>
+                        <Form.Label className="main-signup-form__label">Password:</Form.Label>
                         <Form.Control type="password"
                                       placeholder="Enter password"
                                       name="password" />
