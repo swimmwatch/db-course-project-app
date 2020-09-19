a8d376dcb9a0f616523dbe80d8edd258cfcaac05
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-19 17:53:20 +0300

    Create header component

---
+++ src/client/apps/main/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+import {LinkContainer} from "react-router-bootstrap";
+
+const Header = () => {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <LinkContainer to="/">
+                <Navbar.Brand>PassQuiz</Navbar.Brand>
+            </LinkContainer>
+            <Nav>
+                <LinkContainer to="/signup">
+                    <Nav.Link>Sign Up</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                    <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+            </Nav>
+        </Navbar>
+    );
+}
+
+export default Header;
