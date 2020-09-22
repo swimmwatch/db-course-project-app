7e1ff3d28fc1e0b1510f0d1b59252ae659545398
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-22 15:49:11 +0300

    Add menu for TestCard component

--- src/client/apps/main/components/TestCard/TestCard.jsx
+++ src/client/apps/main/components/TestCard/TestCard.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import PropType from "prop-types";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
 
 import "./style.scss";
 
@@ -11,8 +12,18 @@ const TestCard = ({ title, description }) => {
             <Card.Body>
                 <Card.Title className="test-card__title">{title}</Card.Title>
                 <Card.Text className="test-card__description">{description}</Card.Text>
+
                 <Button className="test-card__pass-btn"
                         variant="primary">Pass test</Button>
+
+                <Dropdown className="test-card__dropdown-menu">
+                    <Dropdown.Toggle variant="primary">Menu</Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item as="button">Edit</Dropdown.Item>
+                        <Dropdown.Item as="button">Delete</Dropdown.Item>
+                        <Dropdown.Item as="button">Something else</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </Card.Body>
         </Card>
     );
