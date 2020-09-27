50db1cf3ec117fc9d69e9ff9833a2fdf7f3f30d4
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-27 11:16:56 +0300

    Fix form data handing from client

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,12 @@
+import multer from "multer";
 import { Router } from "express";
 import * as authController from "../controllers/auth";
 
+const upload = multer();
+
 const router = new Router();
 
-router.post('/signup', authController.signup);
-router.post('/signin', authController.signin);
+router.post('/signup', upload.none(), authController.signup);
+router.post('/signin', upload.none(), authController.signin);
 
 export default router;
