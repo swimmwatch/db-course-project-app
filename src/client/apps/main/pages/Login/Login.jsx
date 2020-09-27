624ccd8871f2f2c9cd7479749039aafe8e9aa26e
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-27 10:06:58 +0300

    Remake Login page to stateful component

--- src/client/apps/main/pages/Login/Login.jsx
+++ src/client/apps/main/pages/Login/Login.jsx
@@ -8,37 +8,37 @@ import LinkContainer from "react-router-bootstrap/lib/LinkContainer";
 
 import "./style.scss";
 
-const Login = () => {
-    return (
-        <Container className="p-3">
-            <Col lg={{offset: 3, span: 6}}>
-                <h2 className="main-login-form__title">Login form</h2>
-                <Form>
-                    <Form.Group controlId="main-login-form__login">
-                        <Form.Label className="main-login-form__label">Login:</Form.Label>
-                        <Form.Control type="text"
-                                      placeholder="Enter login"
-                                      name="login"/>
-                    </Form.Group>
-                    <Form.Group controlId="main-login-form__password">
-                        <Form.Label className="main-login-form__label">Password:</Form.Label>
-                        <Form.Control type="password"
-                                      placeholder="Enter password"
-                                      name="password"/>
-                    </Form.Group>
-                    <Form.Group controlId="main-login-form__checkbox">
-                        <Form.Check type="checkbox" label="Remember me"/>
-                    </Form.Group>
-                    <Button variant="primary"
-                            type="submit"
-                            block>Submit</Button>
-                    <LinkContainer to="#">
-                        <a className="main-login-form__forgot-password">Forgot password?</a>
-                    </LinkContainer>
-                </Form>
-            </Col>
-        </Container>
-    );
-};
-
-export default Login;
+export default class Login extends React.Component {
+    render() {
+        return (
+            <Container className="p-3">
+                <Col lg={{offset: 3, span: 6}}>
+                    <h2 className="main-login-form__title">Login form</h2>
+                    <Form>
+                        <Form.Group controlId="main-login-form__login">
+                            <Form.Label className="main-login-form__label">Login:</Form.Label>
+                            <Form.Control type="text"
+                                          placeholder="Enter login"
+                                          name="login"/>
+                        </Form.Group>
+                        <Form.Group controlId="main-login-form__password">
+                            <Form.Label className="main-login-form__label">Password:</Form.Label>
+                            <Form.Control type="password"
+                                          placeholder="Enter password"
+                                          name="password"/>
+                        </Form.Group>
+                        <Form.Group controlId="main-login-form__checkbox">
+                            <Form.Check type="checkbox" label="Remember me"/>
+                        </Form.Group>
+                        <Button variant="primary"
+                                type="submit"
+                                block>Submit</Button>
+                        <LinkContainer to="#">
+                            <a className="main-login-form__forgot-password">Forgot password?</a>
+                        </LinkContainer>
+                    </Form>
+                </Col>
+            </Container>
+        );
+    }
+}
