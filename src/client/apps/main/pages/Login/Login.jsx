3f5f8acb66a5018f2fd9096e4391908335cb2b96
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-27 11:31:54 +0300

    Handle login form reading data

--- src/client/apps/main/pages/Login/Login.jsx
+++ src/client/apps/main/pages/Login/Login.jsx
@@ -9,6 +9,31 @@ import LinkContainer from "react-router-bootstrap/lib/LinkContainer";
 import "./style.scss";
 
 export default class Login extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            login: '',
+            password: '',
+            readMeChecked: false
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value });
+    }
+
+    handleCheckboxChange(event) {
+        const { name, checked } = event.target;
+
+        this.setState({ [name]: checked });
+    }
+
     render() {
         return (
             <Container className="p-3">
@@ -19,16 +44,21 @@ export default class Login extends React.Component {
                             <Form.Label className="main-login-form__label">Login:</Form.Label>
                             <Form.Control type="text"
                                           placeholder="Enter login"
-                                          name="login"/>
+                                          name="login"
+                                          onChange={this.handleInputChange} />
                         </Form.Group>
                         <Form.Group controlId="main-login-form__password">
                             <Form.Label className="main-login-form__label">Password:</Form.Label>
                             <Form.Control type="password"
                                           placeholder="Enter password"
-                                          name="password"/>
+                                          name="password"
+                                          onChange={this.handleInputChange}/>
                         </Form.Group>
                         <Form.Group controlId="main-login-form__checkbox">
-                            <Form.Check type="checkbox" label="Remember me"/>
+                            <Form.Check type="checkbox"
+                                        label="Remember me"
+                                        name="readMeChecked"
+                                        onChange={this.handleCheckboxChange} />
                         </Form.Group>
                         <Button variant="primary"
                                 type="submit"
