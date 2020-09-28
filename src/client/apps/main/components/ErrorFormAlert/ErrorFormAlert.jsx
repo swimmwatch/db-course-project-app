57fb1e5b314412517470da480fad817c4f19594f
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-28 11:56:14 +0300

    Make ErrorFormAlert component to stateless

--- src/client/apps/main/components/ErrorFormAlert/ErrorFormAlert.jsx
+++ src/client/apps/main/components/ErrorFormAlert/ErrorFormAlert.jsx
@@ -1,34 +1,25 @@
 import * as React from "react";
 import PropTypes from "prop-types";
+
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 
-class ErrorFormAlert extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            show: false
-        };
-    }
-
-    render() {
-        return (
-            <Alert variant="danger" show={this.props.show}>
-                <Alert.Heading>You got an error!</Alert.Heading>
-                <ul>
-                    {
-                        this.props.listErrors.map(({ message }, i) => <li key={i}>{message}</li>)
-                    }
-                </ul>
-                <hr/>
-                <div className="d-flex justify-content-end">
-                    <Button variant="outline-danger"
-                            onClick={this.props.onHide}>Ok</Button>
-                </div>
-            </Alert>
-        );
-    }
+const ErrorFormAlert = ({ listErrors, show, onHide }) => {
+    return (
+        <Alert variant="danger" show={show}>
+            <Alert.Heading>You got an error!</Alert.Heading>
+            <ul>
+                {
+                    listErrors.map(({ message }, i) => <li key={i}>{message}</li>)
+                }
+            </ul>
+            <hr/>
+            <div className="d-flex justify-content-end">
+                <Button variant="outline-danger"
+                        onClick={onHide}>Ok</Button>
+            </div>
+        </Alert>
+    );
 }
 
 ErrorFormAlert.propTypes = {
