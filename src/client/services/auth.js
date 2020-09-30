73c169a2316881278b18cb25dacac5a6c1a75f28
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-30 10:52:30 +0300

    Create auth service for client

---
+++ src/client/services/auth.js
@@ -0,0 +1,29 @@
+async function signUp(formData) {
+    const response = await fetch("/api/signup", {
+        method: "POST",
+        body: formData
+    });
+
+    if (response.ok) {
+        return Promise.resolve();
+    } else {
+        throw await response.json();
+    }
+}
+
+async function signIn(formData) {
+    const response = await fetch("/api/signin", {
+        method: "POST",
+        body: formData
+    });
+
+    const responseJson = await response.json();
+
+    if (response.ok) {
+        return Promise.resolve(responseJson)
+    } else {
+        throw responseJson;
+    }
+}
+
+export default { signUp, signIn };
