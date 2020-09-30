1052ed6d87a757e8d95aa58695cc5a5e76de7685
Dmitry <vasiliev.dim2014@gmail.com>
2020-09-30 12:39:40 +0300

    Add modal window to header

--- src/client/apps/main/components/Header/Header.jsx
+++ src/client/apps/main/components/Header/Header.jsx
@@ -3,12 +3,20 @@ import * as React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 import { LinkContainer } from "react-router-bootstrap";
 
 import "./style.scss";
 
 const Header = () => {
+    const [modalShow, setModalShow] = React.useState(false);
+
+    const showModal = () => setModalShow(true);
+
+    const hideModal = () => setModalShow(false);
+
     return (
         <Navbar bg="dark" variant="dark">
             <LinkContainer to="/">
@@ -26,11 +34,29 @@ const Header = () => {
                         <NavDropdown.Item>My profile</NavDropdown.Item>
                     </LinkContainer>
                     <NavDropdown.Divider />
-                    <LinkContainer to="/logout">
-                        <NavDropdown.Item>Logout</NavDropdown.Item>
+                    <LinkContainer to="#">
+                        <NavDropdown.Item onClick={showModal}>Logout</NavDropdown.Item>
                     </LinkContainer>
                 </NavDropdown>
             </Nav>
+
+            <Modal
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                onHide={hideModal}
+                show={modalShow}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">Log out</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>Are you sure you want to log-off?</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={hideModal}>Yes</Button>
+                </Modal.Footer>
+            </Modal>
         </Navbar>
     );
 }
