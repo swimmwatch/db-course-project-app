987d4b24ff55ad7ab5a7d33efc621cc8b4ff19cf
Dmitry <vasiliev.dim2014@gmail.com>
2020-10-01 13:24:32 +0300

    Fix http error info component

--- src/client/apps/main/components/ErrorInfo/HttpErrorInfo.jsx
+++ src/client/apps/main/components/ErrorInfo/HttpErrorInfo.jsx
@@ -1,16 +1,18 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import { ReasonPhrases } from "http-status-codes";
+import { getStatusText } from "http-status-codes";
 
 import "./style.scss";
 
 const HttpErrorInfo = ({ status, reason }) => {
-    const reasonText = reason ? reason : ReasonPhrases[status];
+    const reasonText = reason ? reason : getStatusText(status);
 
     return (
         <div className="http-error-info">
-            <h3 className="http-error-info__status">{status}</h3>
-            <p className="http-error-info">{reasonText}</p>
+            <div className="http-error-info__box">
+                <h3 className="http-error-info__status">{status}</h3>
+                <p className="http-error-info__reason">{reasonText}</p>
+            </div>
         </div>
     );
 };
