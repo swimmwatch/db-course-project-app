517203dc698e0f1f8bab2739a7d1aaba1d6bddf1
Dmitry <vasiliev.dim2014@gmail.com>
2020-10-02 19:30:22 +0300

    Connect Profile page to global state

--- src/client/apps/main/pages/Profile/Profile.jsx
+++ src/client/apps/main/pages/Profile/Profile.jsx
@@ -1,6 +1,8 @@
 import { LoremIpsum } from "lorem-ipsum";
 import { random } from "lodash";
 import * as React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import {NOT_FOUND} from "http-status-codes";
 
 import { Switch, Route } from 'react-router-dom';
@@ -39,43 +41,59 @@ for (let i = 0; i < 7; i++) {
     });
 }
 
-const Profile = () => (
-    <Container className="p-3">
-        <Row>
-            <Col lg={3}>
-                <img className="profile__avatar"
-                     src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" alt="avatar"/>
-                <p className="profile__username">Profile name</p>
-            </Col>
-            <Col lg={9}>
-                <Nav variant="tabs" defaultActiveKey="tests">
-                    <Nav.Item>
-                        <LinkContainer to="/profile/tests">
-                            <Nav.Link eventKey="tests">My tests</Nav.Link>
-                        </LinkContainer>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <LinkContainer to="/profile/attempts">
-                            <Nav.Link eventKey="attempts">My attempts</Nav.Link>
-                        </LinkContainer>
-                    </Nav.Item>
-                    <Nav.Item>
-                        <LinkContainer to="/profile/settings">
-                            <Nav.Link eventKey="settings">Settings</Nav.Link>
-                        </LinkContainer>
-                    </Nav.Item>
-                </Nav>
+const Profile = ({ user }) => {
+    return (
+        <Container className="p-3">
+            <Row>
+                <Col lg={3}>
+                    <img className="profile__avatar"
+                         src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" alt="avatar"/>
+                    <p className="profile__username">{ user.login }</p>
+                </Col>
+                <Col lg={9}>
+                    <Nav variant="tabs" defaultActiveKey="tests">
+                        <Nav.Item>
+                            <LinkContainer to="/profile/tests">
+                                <Nav.Link eventKey="tests">My tests</Nav.Link>
+                            </LinkContainer>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <LinkContainer to="/profile/attempts">
+                                <Nav.Link eventKey="attempts">My attempts</Nav.Link>
+                            </LinkContainer>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <LinkContainer to="/profile/settings">
+                                <Nav.Link eventKey="settings">Settings</Nav.Link>
+                            </LinkContainer>
+                        </Nav.Item>
+                    </Nav>
 
-                <Switch>
-                    <Route exact path="/profile" render={() => <ListTestCards tests={tests} />} />
-                    <Route path="/profile/tests" render={() => <ListTestCards tests={tests} />} />
-                    <Route path="/profile/attempts" component={ProfileAttempts} />
-                    <Route path="/profile/settings" component={ProfileSettings} />
-                    <Route component={() => <HttpErrorInfo status={NOT_FOUND} />} />
-                </Switch>
-            </Col>
-        </Row>
-    </Container>
-);
+                    <Switch>
+                        <Route exact path="/profile" render={() => <ListTestCards tests={tests} />} />
+                        <Route path="/profile/tests" render={() => <ListTestCards tests={tests} />} />
+                        <Route path="/profile/attempts" component={ProfileAttempts} />
+                        <Route path="/profile/settings" component={ProfileSettings} />
+                        <Route component={() => <HttpErrorInfo status={NOT_FOUND} />} />
+                    </Switch>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
 
-export default Profile;
+Profile.propTypes = {
+    user: PropTypes.shape({
+        login: PropTypes.string
+    })
+};
+
+function mapStateToProps(state) {
+    const { user } = state.auth;
+
+    return { user };
+}
+
+const connectedProfile = connect(mapStateToProps)(Profile);
+
+export { connectedProfile as Profile };
