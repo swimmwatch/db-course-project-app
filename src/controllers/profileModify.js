18418b4aec2c382171d71cac59e2714d3a994849
Dmitry <vasiliev.dim2014@gmail.com>
2020-10-04 22:12:01 +0300

    Create controller that remove user

--- src/controllers/profileModify.js
+++ src/controllers/profileModify.js
@@ -1,7 +1,16 @@
+import User from "../models/User";
+import {OK} from "http-status-codes";
+
 export const updatePassword = async (req, res) => {
     res.end();
 };
 
 export const remove = async (req, res) => {
-    res.end();
+    const { user_id } = req;
+
+    const user = await User.findByPk(user_id);
+
+    user.destroy();
+
+    res.sendStatus(OK);
 };
