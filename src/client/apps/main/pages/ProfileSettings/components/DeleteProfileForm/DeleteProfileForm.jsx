0f4dc88ef9f047ee01352e329d494ac3db232c05
Dmitry <vasiliev.dim2014@gmail.com>
2020-10-04 22:13:20 +0300

    Handle deleting user

--- src/client/apps/main/pages/ProfileSettings/components/DeleteProfileForm/DeleteProfileForm.jsx
+++ src/client/apps/main/pages/ProfileSettings/components/DeleteProfileForm/DeleteProfileForm.jsx
@@ -1,7 +1,12 @@
 import * as React from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import ReactRouterPropTypes from "react-router-prop-types";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import * as authActions from "../../../../../../actions/auth";
+import * as editProfileSettings from "../../../../../../services/editProfileSettings";
 
 class DeleteProfileForm extends React.Component {
     constructor(props) {
@@ -11,7 +16,7 @@ class DeleteProfileForm extends React.Component {
             modalShow: false
         }
 
-        // this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.toggleWarningModal = this.toggleWarningModal.bind(this);
     }
 
@@ -21,6 +26,20 @@ class DeleteProfileForm extends React.Component {
                 modalShow: !prev.modalShow
             };
         });
+    }
+
+    async handleSubmit() {
+        const { history, dispatch } = this.props;
+
+        try {
+            await editProfileSettings.remove();
+
+            dispatch(authActions.logOut());
+
+            history.push('/');
+        } catch (err) {
+            console.log(err);
+        }
     }
 
     render() {
@@ -46,7 +65,7 @@ class DeleteProfileForm extends React.Component {
                         <p>Are you sure you want to delete profile?</p>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button>Yes</Button>
+                        <Button onClick={this.handleSubmit}>Yes</Button>
                     </Modal.Footer>
                 </Modal>
             </>
@@ -55,7 +74,11 @@ class DeleteProfileForm extends React.Component {
 }
 
 DeleteProfileForm.propTypes = {
-    onSubmitError: PropTypes.func
+    onSubmitError: PropTypes.func,
+    history: ReactRouterPropTypes.history,
+    dispatch: PropTypes.func
 };
 
-export default DeleteProfileForm;
+const connectedDeleteProfileForm = connect()(DeleteProfileForm);
+
+export default withRouter(connectedDeleteProfileForm);
