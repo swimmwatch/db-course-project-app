0098f7895c7f667abbed1dc7fd9a07860f66b079
Dmitry <vasiliev.dim2014@gmail.com>
2020-10-06 23:05:26 +0300

    Add svg icon trash to delete button in AnswerEditItem

--- src/client/apps/main/components/AnswerEditItem/AnswerEditItem.jsx
+++ src/client/apps/main/components/AnswerEditItem/AnswerEditItem.jsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 import "./style.scss";
 
@@ -16,6 +19,11 @@ class AnswerEditItem extends React.Component {
                     <InputGroup.Radio name="1" aria-label="Radio button for following text input" />
                 </InputGroup.Prepend>
                 <FormControl aria-label="Text input with radio button" />
+                <InputGroup.Append>
+                    <Button variant="danger">
+                        <FontAwesomeIcon icon={faTrash} />
+                    </Button>
+                </InputGroup.Append>
             </InputGroup>
         );
     }
