82ee8c9f062bf01efd9ca09d1acd8549a1725e7e
Dmitry <vasiliev.dim2014@gmail.com>
2020-10-06 12:34:38 +0300

    Add basic markup for test editor page

--- src/client/apps/main/pages/TestEditor/TestEditor.jsx
+++ src/client/apps/main/pages/TestEditor/TestEditor.jsx
@@ -3,6 +3,18 @@ import PropTypes from "prop-types";
 import ReactRouterPropTypes from "react-router-prop-types";
 import { connect } from "react-redux";
 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import ToggleButton from "react-bootstrap/ToggleButton";
+import ToggleButtonGroup from "react-bootstrap/ToggleButtonGroup";
+import Button from "react-bootstrap/Button";
+
+import "./style.scss";
+
 class TestEditor extends React.Component {
     constructor(props) {
         super(props);
@@ -10,7 +22,121 @@ class TestEditor extends React.Component {
 
     render() {
         return (
-            <h2>hello</h2>
+            <Container className="p-3">
+                <Form>
+                    <Row>
+                        <Col lg={3}>
+                            <Form.Label>Title</Form.Label>
+                        </Col>
+                        <Col lg={6}>
+                            <Form.Group controlId="">
+                                <Form.Control />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={3}>
+                            <Form.Label>Description</Form.Label>
+                        </Col>
+                        <Col lg={6}>
+                            <Form.Group controlId="">
+                                <Form.Control as="textarea" rows={3} />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={3}>
+                            <Form.Label>Tags</Form.Label>
+                        </Col>
+                        <Col lg={6}>
+                            <Form.Group controlId="">
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                        aria-label="Recipient's username"
+                                        aria-describedby="basic-addon2"
+                                    />
+                                    <InputGroup.Append>
+                                        <Button variant="primary">Add</Button>
+                                    </InputGroup.Append>
+                                </InputGroup>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+
+                    <hr/>
+
+                    <Row>
+                        <Col lg={3}>
+                            <Form.Label>Question</Form.Label>
+                        </Col>
+                        <Col lg={6}>
+                            <Form.Group controlId="">
+                                <Form.Control as="textarea" rows={3} />
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={3}>
+                            <Form.Label>Question type</Form.Label>
+                        </Col>
+                        <Col lg={6}>
+                            <Form.Group controlId="">
+                                <ToggleButtonGroup type="radio" name="options" defaultValue={1}>
+                                    <ToggleButton value={1}>One answer</ToggleButton>
+                                    <ToggleButton value={2}>Multiple answers</ToggleButton>
+                                </ToggleButtonGroup>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={3}>
+                            <Form.Label>Answers</Form.Label>
+                        </Col>
+                        <Col lg={6}>
+                            <Form.Group controlId="1">
+                                <InputGroup>
+                                    <InputGroup.Prepend>
+                                        <Form.Check
+                                            type="radio"
+                                            name="formHorizontalRadios"
+                                            id="formHorizontalRadios1"
+                                        />
+                                    </InputGroup.Prepend>
+                                    <FormControl />
+                                </InputGroup>
+                                <InputGroup>
+                                    <InputGroup.Prepend>
+                                        <Form.Check
+                                            type="radio"
+                                            name="formHorizontalRadios"
+                                            id="formHorizontalRadios1"
+                                        />
+                                    </InputGroup.Prepend>
+                                    <FormControl />
+                                </InputGroup>
+                                <InputGroup>
+                                    <InputGroup.Prepend>
+                                        <Form.Check
+                                            type="radio"
+                                            name="formHorizontalRadios"
+                                            id="formHorizontalRadios1"
+                                        />
+                                    </InputGroup.Prepend>
+                                    <FormControl />
+                                </InputGroup>
+                            </Form.Group>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={{offset: 3, span: 6}} >
+                            <Button className="test-editor__submit-btn"
+                                    type="primary">
+                                Save
+                            </Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </Container>
         );
     }
 }
