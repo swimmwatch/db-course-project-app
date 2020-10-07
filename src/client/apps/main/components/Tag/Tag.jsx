9d1ea8a6fcc77d67944240cff221b9da3928fb4b
Dmitry <vasiliev.dim2014@gmail.com>
2020-10-07 19:53:33 +0300

    Update tag design

--- src/client/apps/main/components/Tag/Tag.jsx
+++ src/client/apps/main/components/Tag/Tag.jsx
@@ -1,11 +1,18 @@
 import * as React from "react";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import "./style.scss";
 
 const Tag = ({ content }) => {
     return (
-        <li className="tag">{content}</li>
+        <li className="tag">
+            <span className="tag__content">{content}</span>
+            <button className="tag__delete-btn">
+                <FontAwesomeIcon icon={faTimes} />
+            </button>
+        </li>
     );
 };
 
