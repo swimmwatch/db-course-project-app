7626d02092c494ad6763b54e77cf33030daa132d
Dmitry <vasiliev.dim2014@gmail.com>
2020-10-11 21:39:58 +0300

    Update ListTestCards component

--- src/client/apps/main/components/ListTestCards/ListTestCards.jsx
+++ src/client/apps/main/components/ListTestCards/ListTestCards.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import TestCard from "../TestCard";
@@ -10,21 +9,19 @@ import "./style.scss";
 
 const ListTestCards = ({ tests }) => {
     return (
-        <Container className="p-3">
-            <Row>
-                {
-                    tests.map((el, i) => {
-                        const { title, description, author } = el;
+        <Row>
+            {
+                tests.map((el, i) => {
+                    const { title, description, author } = el;
 
-                        return (
-                            <Col lg={12} key={i} className="list-tests__col">
-                                <TestCard title={title} description={description} author={author} />
-                            </Col>
-                        );
-                    })
-                }
-            </Row>
-        </Container>
+                    return (
+                        <Col lg={12} key={i} className="list-tests__col">
+                            <TestCard title={title} description={description} author={author} />
+                        </Col>
+                    );
+                })
+            }
+        </Row>
     );
 };
 
