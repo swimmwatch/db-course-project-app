86ad9f4d0a9550bae85617a772ec0bd4d63d316e
Dmitry <vasiliev.dim2014@gmail.com>
2020-10-20 14:00:37 +0300

    Create client service for passing tests

---
+++ src/client/services/testPassing.js
@@ -0,0 +1,56 @@
+import {appendAuth} from "../helpers/header";
+import {getToken} from "../helpers/token";
+import {INTERNAL_SERVER_ERROR, NOT_FOUND} from "http-status-codes";
+
+/**
+ * Get test for passing
+ * @param {number} testId - Test ID
+ * @return {Promise<unknown>}
+ */
+export const init = async testId => {
+    const token = getToken();
+    const headers = new Headers();
+
+    appendAuth(headers, token);
+
+    // create url
+    const url = new URL('/api/test/pass'),
+          params = { testId };
+
+    Object.keys(params)
+          .forEach(key => url.searchParams.append(key, params[key]))
+
+    const response = await fetch(url.toString(), {
+        method: 'GET',
+        headers
+    });
+
+    let responseJson = null;
+    try {
+        responseJson = await response.json();
+    } catch (err) {
+        // handle case if json is invalid
+        throw {
+            status: INTERNAL_SERVER_ERROR,
+            message: 'something went wrong'
+        };
+    }
+
+    if (response.ok) {
+        return Promise.resolve(responseJson);
+    } else {
+        switch (response.status) {
+            case NOT_FOUND: {
+                throw {
+                    status: NOT_FOUND,
+                    message: 'test not found'
+                };
+            }
+            default: {
+                // TODO: handle default case
+
+                console.log('something went wrong');
+            }
+        }
+    }
+};
