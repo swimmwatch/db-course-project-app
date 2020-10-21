c30e1585385aa2f45d7101182e1d804803ef6e5e
Dmitry <vasiliev.dim2014@gmail.com>
2020-10-21 19:54:35 +0300

    Add action 'update answer' for test passing reducer

--- src/client/actions/testPassing.js
+++ src/client/actions/testPassing.js
@@ -1,8 +1,18 @@
 export const INIT = "INIT";
+export const UPDATE_ANSWER = "UPDATE_ANSWER";
 
 export const init = internalState => {
     return {
         type: INIT,
         internalState
+    }
+};
+
+export const updateAnswer = (questionId, answerId, state) => {
+    return {
+        type: INIT,
+        questionId,
+        answerId,
+        state
     }
 };
