c0659c3a95750c41ec37e62c6c79114e17195942
Dmitry <vasiliev.dim2014@gmail.com>
2020-11-03 12:45:17 +0300

    Handle search input reading in profile tests page

--- src/client/apps/main/pages/ProfileTests/ProfileTests.jsx
+++ src/client/apps/main/pages/ProfileTests/ProfileTests.jsx
@@ -4,6 +4,10 @@ import ListTestCards from "../../components/ListTestCards";
 import * as editTest from "../../../../services/editTest";
 import HttpErrorInfo from "../../components/HttpErrorInfo";
 import {NO_CONTENT} from "http-status-codes";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 import "./style.scss";
 
@@ -12,10 +16,15 @@ class ProfileTests extends React.Component {
         super(props);
 
         this.state = {
-            profileTests: []
+            profileTests: [],
+            searchTitle: '',
+            searchTagValue: '',
+            searchTags: []
         };
 
         this.handleDeleteTestCard = this.handleDeleteTestCard.bind(this);
+        this.handleSearchTitleChange = this.handleSearchTitleChange.bind(this);
+        this.handleSearchTagValueChange = this.handleSearchTagValueChange.bind(this);
     }
 
     async handleDeleteTestCard(testId) {
@@ -37,6 +46,18 @@ class ProfileTests extends React.Component {
         });
     }
 
+    handleSearchTitleChange({ target }) {
+        const { value } = target;
+
+        this.setState({ searchTitle: value });
+    }
+
+    handleSearchTagValueChange({ target }) {
+        const { value } = target;
+
+        this.setState({ searchTagValue: value })
+    }
+
     async componentDidMount() {
         const responseJson = await editTest.getOwnTests();
 
@@ -48,6 +69,37 @@ class ProfileTests extends React.Component {
 
         return (
             <Container className="p-3">
+                <Form>
+                    <Form.Group controlId="">
+                        <Form.Label>Test title:</Form.Label>
+                        <InputGroup className="mb-3">
+                            <FormControl
+                                aria-label="test title"
+                                aria-describedby="basic-addon2"
+                                required
+                                onChange={this.handleSearchTitleChange}
+                            />
+                            <InputGroup.Append>
+                                <Button variant="primary">Find</Button>
+                            </InputGroup.Append>
+                        </InputGroup>
+                    </Form.Group>
+                    <Form.Group controlId="">
+                        <Form.Label>Tags:</Form.Label>
+                        <InputGroup className="mb-3">
+                            <FormControl
+                                aria-label="Recipient's username"
+                                aria-describedby="basic-addon2"
+                                required
+                                onChange={this.handleTagInputChange}
+                            />
+                            <InputGroup.Append>
+                                <Button variant="primary"
+                                        onClick={()=>{}}>Add</Button>
+                            </InputGroup.Append>
+                        </InputGroup>
+                    </Form.Group>
+                </Form>
                 {
                     profileTests.length ? (
                         <ListTestCards tests={profileTests}
