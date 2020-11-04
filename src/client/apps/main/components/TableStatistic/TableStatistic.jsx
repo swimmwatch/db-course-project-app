768e9068f2b13f3fd4f8a1828ecfefe225303f91
Dmitry <vasiliev.dim2014@gmail.com>
2020-11-04 21:18:37 +0300

    Create TableStatistic component

---
+++ src/client/apps/main/components/TableStatistic/TableStatistic.jsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import PropTypes from "prop-types";
+import Container from "react-bootstrap/Container";
+import { MDBDataTable } from 'mdbreact';
+
+
+const TableStatistic = ({rows}) => {
+    const data = {
+        columns: [
+            {
+                label: 'Attempt ID',
+                field: 'attemptId',
+                sort: 'asc',
+            },
+            {
+                label: 'User',
+                field: 'user',
+                sort: 'asc',
+            },
+            {
+                label: 'Result',
+                field: 'result',
+                sort: 'asc',
+            },
+            {
+                label: 'Date',
+                field: 'date',
+                sort: 'asc',
+            }
+        ],
+        rows: rows
+    };
+
+    return (
+        <Container className="p-3">
+            <MDBDataTable
+                striped
+                bordered
+                small
+                data={data}
+            />
+        </Container>
+    );
+};
+
+TableStatistic.propTypes = {
+    rows: PropTypes.arrayOf(
+        PropTypes.exact({
+            attemptId: PropTypes.number,
+            user: PropTypes.string,
+            result: PropTypes.number,
+            date: PropTypes.string
+        })
+    ).isRequired
+};
+
+export default TableStatistic;
