a5ae93d4f008d7b0d7bcaa9621aefd3bdd079798
Dmitry <vasiliev.dim2014@gmail.com>
2020-11-06 23:03:38 +0300

    Fix routing

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,7 +3,9 @@ import { Router } from "express";
 const router = new Router();
 
 router.get("*", (req, res) => {
-    res.render("index");
+    const isElectronApp = req.header('user-agent').includes('Electron');
+
+    res.render(isElectronApp ? "contest" : "index");
 });
 
 export default router;
